d897e04fab01be721c9cfb558e740ea207f3c293
janmusil <janmusil@Jan--Mac-mini.local>
2024-12-09 13:51:29 +0100

    first commit

---
+++ src/test/java/cz/uhk/ppro_projekt/PproProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.uhk.ppro_projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PproProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
